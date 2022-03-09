71785141be0772b759d281b182a47a4353279b56
harshitbansal1602 <hharshitbansal@gmail.com>
2022-03-09 23:05:40 +0530

    Fixed bugs caused due to newer version of router-react-dom had breaking changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Container } from '@material-ui/core';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Container } from "@material-ui/core";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from './components/Home/Home';
-import Navbar from './components/Navbar/Navbar';
-import Auth from './components/Auth/Auth';
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Auth from "./components/Auth/Auth";
 
 const App = () => (
   <BrowserRouter>
     <Container maxWidth="lg">
       <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/auth" exact component={Auth} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/auth" element={<Auth />}></Route>
+      </Routes>
     </Container>
   </BrowserRouter>
 );
